ddb549c52845531c5ed0f84a28058f5cf0e65ed7
Agbedejobi <nigeriang@hotmail.com>
2019-08-14 20:14:50 -0300

    Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,22 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var anyway = [ 'stalker', true , 5 , null , 'come here' ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function voltarArray(aha) {
+
+	return aha;
+
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+voltarArray(aha)[1];
+
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
