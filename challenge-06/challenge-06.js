ccf460991c5afbf541aaf5db56f8995d6fcb3a03
Samuel Agbedejobi <nigeriang@hotmail.com>
2019-08-19 14:59:31 -0300

    Challenge 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -14,7 +14,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-var teams = [ 'Corinthians', 'Palmeiras' , 'Portugueasa', 'São Paulo', 'Portugueasa'];
+var teams = [ 'Corinthians', 'Palmeiras' , 'Santos', 'São Paulo', 'Portuguesa'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -37,20 +37,38 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
 function showTeamPosition(a){
 
     var indice = a+1;
+
+return a<=4 ? 'O time que está em ' + indice + 'ºlugar é o ' + teams[a] + '.':'Não temos a informação do time que está no ' + indice +'ºlugar.';
+    /*if(a <= 4) {
+    
     return 'O time que está em ' + indice + 'ºlugar é o ' + teams[a] + '.';
+    } else {
+
+            return 'Não temos a informação do time que está nessa posição.'
+
+    };*/
 };
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log (showTeamPosition(0));
+console.log (showTeamPosition(2));
+console.log (showTeamPosition(3));
+console.log (showTeamPosition(6));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+while(counter >= 20 && counter <= 30){
+    
+    console.log(counter); //console.log(counter++);
+    counter++;
+
+};
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -64,9 +82,55 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(cor) {
+
+
+var collorB = cor === 'blue'? '#0000ff' :  'Não temos o equivalente hexadecimal para ' + cor + '.';
+var collorR = cor === 'red' ? '#ff0000' : 'Não temos o equivalente hexadecimal para ' + cor + '.';
+var collorP = cor === 'black'?'#000000' : 'Não temos o equivalente hexadecimal para ' + cor + '.';
+var collorG = cor === 'pink'? '#ff69b4' : 'Não temos o equivalente hexadecimal para ' + cor + '.';
+var collorM = cor === 'brown'?'#964b00' : 'Não temos o equivalente hexadecimal para ' + cor + '.';
+
+
+
+    switch(cor){
+        case 'blue':
+        //console.log('O hexadecimal para a cor ' + cor + ' é ' + collorB +'.');
+        return 'O hexadecimal para a cor ' + cor + ' é ' + collorB +'.';
+        break;
+        case 'red':
+        //console.log('O hexadecimal para a cor ' + cor + ' é ' + collorR +'.');
+        return 'O hexadecimal para a cor ' + cor + ' é ' + collorR +'.';
+        break;
+        case 'black':
+        //console.log('O hexadecimal para a cor ' + cor + ' é ' + collorP +'.');
+        return 'O hexadecimal para a cor ' + cor + ' é ' + collorP +'.';
+        break;
+        case 'pink':
+        //console.log('O hexadecimal para a cor ' + cor + ' é ' + collorG +'.');
+        return 'O hexadecimal para a cor ' + cor + ' é ' + collorG +'.';
+        break;
+        case 'brown':
+        //console.log('O hexadecimal para a cor ' + cor + ' é ' + collorM +'.');
+        return 'O hexadecimal para a cor ' + cor + ' é ' + collorM +'.';
+        break;
+        default:
+        return 'Não temos o equivalente hexadecimal para ' + cor + '.';
+        //console.log('Não temos o equivalente hexadecimal para ' + cor + '.');
+        //console.log(collor);
+    };
+
+
+};
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+console.log(convertToHex('red'));
+console.log(convertToHex('yellow'));
+console.log(convertToHex('blue'));
+console.log(convertToHex('grey'));
+console.log(convertToHex('black'));
+console.log(convertToHex('pink'));
+console.log(convertToHex('green'));
+console.log(convertToHex('brown'));
