2b1b59bb2f09515d4a3bc1192287a7fe51170d05
Samuel Agbedejobi <nigeriang@hotmail.com>
2019-09-03 17:51:10 -0300

    Challenge-08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -35,14 +35,14 @@ function showName (){
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName();
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log('A função ' + showName.name + ' retorna ' + varShowName + '.');
+console.log('A função ' + varShowName.name + ' retorna ' + varShowName() + '.');
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -71,7 +71,7 @@ function calculator ( a ){
 			a === '/' ? resultado = x / y :'';
 			a === '%' ? resultado = x % y :'';
 			return 'Resultado da operação: ' + x + ' ' + a  + ' ' + y + ' é = ' + resultado + '.';
-		}
+		};
 	
 	}
 
