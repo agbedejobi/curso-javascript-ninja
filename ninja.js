d9e9a581892ce56b78db7cd5b5f8ff2892480606
Samuel Agbedejobi <nigeriang@hotmail.com>
2019-09-05 15:11:45 -0300

    Challenge-09

---
+++ ninja.js
@@ -0,0 +1,8 @@
+function myFunction() {
+	console.log( 'Antes de declarar' , number1);
+	var number1 = 10;
+	console.log('Depois de Declarar' , number1);
+}
+
+console.log( myFunction() );
+
---
+++ node.js
@@ -0,0 +1,7 @@
+function myFunction() {
+	colsole.log('Abtes de criar' ,number1 );
+	var number1 = 10;
+	console.log('Depois de declarar' , number1);
+
+
+	
